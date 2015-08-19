74f20283dd8dff154cc264c97d1ab1155d9cdc2f
ryan <ryantxu@gmail.com>
2015-08-19 13:41:21 -0700

    scaffolding from yo gulp-angular

---
+++ src/app/components/navbar/navbar.directive.ts
@@ -0,0 +1,28 @@
+module mapSearch {
+  'use strict';
+
+  /** @ngInject */
+  export function acmeNavbar(): ng.IDirective {
+
+    return {
+      restrict: 'E',
+      scope: {
+        creationDate: '='
+      },
+      templateUrl: 'app/components/navbar/navbar.html',
+      controller: NavbarController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+  }
+
+  /** @ngInject */
+  class NavbarController {
+    public relativeDate: string;
+
+    constructor(moment: moment.MomentStatic) {
+      this.relativeDate = moment(1440016488687).fromNow();
+    }
+  }
+}
