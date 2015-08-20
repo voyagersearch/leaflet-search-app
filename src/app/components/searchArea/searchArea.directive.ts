143057990fee430c0c76570b27af87c334d6420a
ryan <ryantxu@gmail.com>
2015-08-19 17:12:27 -0700

    search is working!

--- src/app/components/searchArea/searchArea.directive.ts
+++ src/app/components/searchArea/searchArea.directive.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../../.tmp/typings/angularjs/angular.d.ts" />
+/// <reference path="../../../../.tmp/typings/tsd.d.ts" />
 
 module mapSearch {
   'use strict';
@@ -19,12 +19,99 @@ module mapSearch {
 
   }
 
+
+
   /** @ngInject */
   class SearchAreaController {
-    public relativeDate: string;
+    public searchConfig: any;
+    public searchRsp: any;
+    public facets: Array<any>;
 
-    constructor(moment: moment.MomentStatic) {
-      this.relativeDate = moment(1440016488687).fromNow();
+    private animationsEnabled: boolean;
+
+    private $log: ng.ILogService;
+    private $http: ng.IHttpService;
+    private $filter: ng.IFilterService;
+    private $modal: ng.ui.bootstrap.IModalService;
+
+
+    /** @ngInject */
+    constructor($log: ng.ILogService, $http: ng.IHttpService, $filter: ng.IFilterService, $modal: ng.ui.bootstrap.IModalService) {
+      this.$log = $log;
+      this.$http = $http;
+      this.$filter = $filter;
+      this.$modal = $modal;
+
+      this.animationsEnabled = true; // for the popup
+      this.searchConfig = {
+        'url': 'http://voyagerdemo.com/daily/solr/v0/select',
+        'json': {   // end user can *not* change this
+          'params': {
+            'fl': 'name:[name],path:[absolute],format,properties,keywords'
+          },
+          'facet': {
+            'Open In Application': {
+              'terms': {
+                'field': 'format_app',
+                'limit': 5
+              }
+            },
+            'Properties': {
+              'terms': {
+                'field': 'properties',
+                'limit': 5
+              }
+            },
+            'Keywords': {
+              'terms': {
+                'field': 'keywords',
+                'limit': 5
+              }
+            }
+          }
+        },
+        'req': {  // end user can change this
+          start: 0,
+          rows: 50,
+          q: '*:*',
+          fq: []
+        },
+        'view': [
+          {'field': 'name', 'display': 'Name'},
+          {'field': 'path', 'display': 'Path'}
+        ]
+      };
+
+      this.doSearch(); // initalize the response
+    }
+
+    doSearch() {
+
+      var queryString = this.searchConfig.url;
+      queryString += '?json=' + JSON.stringify(this.searchConfig.json);
+      queryString += '&' + jQuery.param(this.searchConfig.req, true); // the user configs
+      queryString += '&wt=json&json.wrf=JSON_CALLBACK';  // jsonp
+
+      // this.$log.info('QUERY', queryString);
+
+      this.$http.jsonp(queryString)
+        .then((response: any) => {
+          this.searchRsp = response.data;
+
+          var theLog = this.$log;
+          var facets = [];
+          angular.forEach(this.searchRsp.facets, function(value, key) {
+            //theLog.info('check', key, value);
+            if (key !== 'count' && key.length > 0) {
+              facets.push( { name: key, value: value} );
+            }
+          });
+          this.facets = facets;
+        })
+        .catch((error: any) => {
+          alert( status + ' ' + queryString );
+         // this.$log.error('XHR To make query.\n', error.data);
+        });
     }
   }
 }
